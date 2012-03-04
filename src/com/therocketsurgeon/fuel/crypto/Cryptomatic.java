d11ccc0d97e2faf77e0885bafc79fe6eb3dfd564
Yakov Khalinsky <yakov@therocketsurgeon.com>
2012-03-04 19:59:55 +1100

    Tidy up methods with try/finally blocks. Throw instead of catch all errors. Added hashMD5() static method. Added hashSHA1() static method. Added examples to ExampleCrypto class.

--- src/com/therocketsurgeon/fuel/crypto/Cryptomatic.java
+++ src/com/therocketsurgeon/fuel/crypto/Cryptomatic.java
@@ -1,16 +1,22 @@
 package com.therocketsurgeon.fuel.crypto;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -20,12 +26,18 @@ import javax.crypto.spec.SecretKeySpec;
 import org.apache.log4j.Logger;
 
 import sun.misc.BASE64Encoder;
-import sun.security.provider.MD5;
 
 import com.therocketsurgeon.fuel.FuelProperty;
 import com.therocketsurgeon.fuel.Property;
 import com.therocketsurgeon.fuel.text.TextUtil;
 
+/**
+ * Class for doing some simple crypto operations.<br/>
+ * This class depends on two lines to be present in a property file in the classpath:<br/>
+ * - com.therocketsurgeon.fuel.crypto.Cryptomatic.salt<br/>
+ * - com.therocketsurgeon.fuel.crypto.Cryptomatic.secret
+ * @author Yakov Khalinsky
+ */
 public class Cryptomatic implements FuelProperty {
 
 	public static Logger log = Logger.getLogger(Cryptomatic.class.getName());
@@ -44,12 +56,31 @@ public class Cryptomatic implements FuelProperty {
 	public static final String PROPERTY_SECRET = Cryptomatic.class.getName() + ".secret";
 	
 	private static SecureRandom RANDOM = new SecureRandom();
-	
-	public Cryptomatic() {
+
+	/**
+	 * Default constructor. This looks for a salt and secret strings in a properties file to be used for subsequent hashing, one and two way encryption methods.
+	 * @throws IllegalArgumentException 
+	 * @throws InvalidAlgorithmParameterException 
+	 * @throws NoSuchPaddingException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeySpecException 
+	 * @throws InvalidKeyException 
+	 */
+	public Cryptomatic() throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalArgumentException {
 		this(Property.getProperty(BUNDLE, PROPERTY_SALT), Property.getProperty(BUNDLE, PROPERTY_SECRET));
 	}
 	
-	public Cryptomatic(String salt, String passphrase) {
+	/**
+	 * Constructor. Allows for salt and passphrase to be set in the arguments.
+	 * @param salt
+	 * @param passphrase
+	 * @throws InvalidAlgorithmParameterException 
+	 * @throws NoSuchPaddingException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeySpecException 
+	 * @throws InvalidKeyException 
+	 */
+	public Cryptomatic(String salt, String passphrase) throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
 		if (!TextUtil.isValid(salt) || !TextUtil.isValid(passphrase)) {
 			throw new IllegalArgumentException("Invalid arguments");
 		}
@@ -58,6 +89,10 @@ public class Cryptomatic implements FuelProperty {
 		initialise();
 	}
 	
+	/**
+	 * Set the encryption algorithm to use in subsequent encryption or hashing methods.
+	 * @param algorithm
+	 */
 	public void setAlgorithm(String algorithm) {
 		if (!TextUtil.isValid(algorithm)) {
 			throw new IllegalArgumentException("Invalid argument");
@@ -65,86 +100,106 @@ public class Cryptomatic implements FuelProperty {
 		this.ALGORITHM = algorithm;
 	}
 	
-	private void initialise() {
+	private void initialise() throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+		KeySpec keySpec = null;
+		SecretKey key = null;
+		AlgorithmParameterSpec paramSpec = null;
 		try {
             // Create the key
-            KeySpec keySpec = new PBEKeySpec(this.PASSPHRASE.toCharArray(), this.SALT.getBytes(), this.iterations);
-            SecretKey key = SecretKeyFactory.getInstance(this.ALGORITHM).generateSecret(keySpec);
+            keySpec = new PBEKeySpec(this.PASSPHRASE.toCharArray(), this.SALT.getBytes(), this.iterations);
+            key = SecretKeyFactory.getInstance(this.ALGORITHM).generateSecret(keySpec);
             
             this.ENCRYPTER = Cipher.getInstance(key.getAlgorithm());
             this.DECRYPTER = Cipher.getInstance(key.getAlgorithm());
 
             // Prepare the parameter to the ciphers
-            AlgorithmParameterSpec paramSpec = new PBEParameterSpec(this.SALT.getBytes(), this.iterations);
+            paramSpec = new PBEParameterSpec(this.SALT.getBytes(), this.iterations);
 
             // Create the ciphers
             this.ENCRYPTER.init(Cipher.ENCRYPT_MODE, key, paramSpec);
             this.DECRYPTER.init(Cipher.DECRYPT_MODE, key, paramSpec);
-        } catch (java.security.InvalidAlgorithmParameterException e) {
-        	log.error(e.getMessage(), e);
-        } catch (java.security.spec.InvalidKeySpecException e) {
-        	log.error(e.getMessage(), e);
-        } catch (javax.crypto.NoSuchPaddingException e) {
-        	log.error(e.getMessage(), e);
-        } catch (java.security.NoSuchAlgorithmException e) {
-        	log.error(e.getMessage(), e);
-        } catch (java.security.InvalidKeyException e) {
-        	log.error(e.getMessage(), e);
+        } finally {
+        	keySpec = null;
+    		key = null;
+    		paramSpec = null;
         }
 	}
 	
-	public String encrypt(String string) {
+	/**
+	 * Encrypts the string. Reversable by calling decrypt().
+	 * @param string
+	 * @return encrypted string
+	 * @throws UnsupportedEncodingException 
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
+	 */
+	public String encrypt(String string) throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
 		if (!TextUtil.isValid(string)) {
 			throw new IllegalArgumentException("Invalid argument");
 		}
-        try {
-            // Encode the string into bytes using utf-8
-            byte[] utf8 = string.getBytes("UTF8");
-
-            // Encrypt
-            byte[] enc = this.ENCRYPTER.doFinal(utf8);
-
-            // Encode bytes to base64 to get a string
-            return new sun.misc.BASE64Encoder().encode(enc);
-        } catch (javax.crypto.BadPaddingException e) {
-        	log.error(e.getMessage(), e);
-        } catch (IllegalBlockSizeException e) {
-        	log.error(e.getMessage(), e);
-        } catch (UnsupportedEncodingException e) {
-        	log.error(e.getMessage(), e);
-        }
-        return null;
+		byte[] utf8 = null;
+		byte[] enc = null;
+		try {
+			utf8 = string.getBytes("UTF8");
+			
+			// Encrypt
+			enc = this.ENCRYPTER.doFinal(utf8);
+			
+			// Encode bytes to base64 to get a string
+			return new sun.misc.BASE64Encoder().encode(enc);
+		} finally {
+			utf8 = null;
+			enc = null;
+		}
     }
 
-    public String decrypt(String string) {
+	/**
+	 * Decrypts the string into its "clear" form as provided in the argument of the encrypt() method.
+	 * @param string
+	 * @return encrypted string
+	 * @throws IOException 
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
+	 */
+    public String decrypt(String string) throws IOException, IllegalBlockSizeException, BadPaddingException {
     	if (!TextUtil.isValid(string)) {
 			throw new IllegalArgumentException("Invalid argument");
 		}
-        try {
-            // Decode base64 to get bytes
-            byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(string);
-
-            // Decrypt
-            byte[] utf8 = this.DECRYPTER.doFinal(dec);
-
-            // Decode using utf-8
-            return new String(utf8, "UTF8");
-        } catch (javax.crypto.BadPaddingException e) {
-        	log.error(e.getMessage(), e);
-        } catch (IllegalBlockSizeException e) {
-        	log.error(e.getMessage(), e);
-        } catch (UnsupportedEncodingException e) {
-        	log.error(e.getMessage(), e);
-        } catch (java.io.IOException e) {
-        	log.error(e.getMessage(), e);
-        }
-        return null;
+    	byte[] dec = null;
+    	byte[] utf8 = null;
+    	try {
+    		// Decode base64 to get bytes
+    		dec = new sun.misc.BASE64Decoder().decodeBuffer(string);
+    		
+    		// Decrypt
+    		utf8 = this.DECRYPTER.doFinal(dec);
+    		
+    		// Decode using utf-8
+    		return new String(utf8, "UTF8");
+    	}
+    	finally {
+    		dec = null;
+        	utf8 = null;
+    	}
     }
 
+    /**
+     * Generate a random string of length 32 characters.
+     * @return String
+     */
     public static String getRandomString() {
     	return new BigInteger(130, RANDOM).toString(32);
     }
     
+    /**
+     * Returns an encoded String from the arguments using the HmacSHA1 algorithm.
+     * @param key
+     * @param data
+     * @return encoded String
+     * @throws NoSuchAlgorithmException
+     * @throws UnsupportedEncodingException
+     * @throws InvalidKeyException
+     */
     public static String getHmacSHA1(String key, String data) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException { 
     	Mac mac = null;
     	BASE64Encoder encoder = null;
@@ -157,6 +212,50 @@ public class Cryptomatic implements FuelProperty {
     		mac = null; 
     		encoder = null; 
     	}
+    }
+    
+    /**
+     * Get MD5 hashed value of String data
+     * @param data
+     * @return hashValue
+     * @throws NoSuchAlgorithmException
+     */
+    public static String hashMD5(String data) throws NoSuchAlgorithmException {
+    	return hash(data, ALGORITHM_MD5);
+    }
+
+    /**
+     * Get SHA1 hashed value of String data
+     * @param data
+     * @return hashValue
+     * @throws NoSuchAlgorithmException
+     */
+    public static String hashSHA1(String data) throws NoSuchAlgorithmException {
+    	return hash(data, ALGORITHM_SHA1);
+    }
+    
+    private static String hash(String data, String algorithm) throws NoSuchAlgorithmException {
+    	if (!TextUtil.isValid(data)) {
+			throw new IllegalArgumentException("Invalid argument");
+		}
+    	StringBuffer hashValue = new StringBuffer(4);
+    	MessageDigest md = null;
+    	String hex = null;
+    	try {
+    		md = MessageDigest.getInstance(algorithm);
+    		byte[] out = md.digest(data.getBytes());
+    		for (int i=0; i < out.length; i++) {
+    			hex = Integer.toHexString(0xff & out[i]);
+    		    if(hex.length() == 1) {
+    		    	hashValue.append('0');
+    		    }
+    		    hashValue.append(hex);
+    		}
+    	} finally {
+    		md = null;
+    		hex = null;
+    	}
+    	return hashValue.toString();
     }
     
 }
--- src/com/therocketsurgeon/fuel/example/ExampleCrypto.java
+++ src/com/therocketsurgeon/fuel/example/ExampleCrypto.java
@@ -7,9 +7,16 @@ public class ExampleCrypto {
 	public static void main(String[] args) {
 
 		try {
-			Cryptomatic crypto = new Cryptomatic();
-			String str = crypto.encrypt("password");
-			System.out.println(str);
+//			Cryptomatic crypto = new Cryptomatic();
+//			String str = crypto.encrypt("password");
+//			System.out.println(str);
+			
+//			String randomString = Cryptomatic.getRandomString(); 
+//			System.out.print(randomString);
+			
+			// String hashValue = Cryptomatic.hashMD5("hash this String");
+			String hashValue = Cryptomatic.hashSHA1("hash this String");
+			System.out.println(hashValue);
 		} catch (Exception ex) {
 			 ex.printStackTrace();
 		} finally {
